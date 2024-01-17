cabd575e61c37b43d86ef164221b84701a12fc07
Dillon Duncan <dillonduncan0607@gmail.com>
2024-01-17 11:46:43 -0500

    pantalla compartir imagenes

--- app/src/main/java/com/example/pantallacompartir/MainActivity.kt
+++ app/src/main/java/com/example/pantallacompartir/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pantallacompartir
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pantallacompartir.databinding.ActivityMainBinding
@@ -12,12 +13,39 @@ class MainActivity : AppCompatActivity() {
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnCompartir.setOnClickListener { compartir() }
+        binding.btnCompartirImagen.setOnClickListener { compartirImagen() }
+        binding.btnCompartirImagenes.setOnClickListener { compartirImagenes() }
     }
+
+    private fun compartirImagenes() {
+        val uriArray:ArrayList<Uri> = arrayListOf(Uri.EMPTY, Uri.EMPTY)
+        val intent=Intent().apply {
+            action=Intent.ACTION_SEND_MULTIPLE
+            putParcelableArrayListExtra(Intent.EXTRA_STREAM, uriArray)
+            type="image/*"
+            putExtra(Intent.EXTRA_TITLE,"Compartir Imagen")
+
+        }
+        startActivity(Intent.createChooser(intent,null))
+    }
+
+    private fun compartirImagen() {
+        val intent=Intent().apply {
+            action=Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_STREAM, Uri.EMPTY)
+            type="image/jpeg"
+            putExtra(Intent.EXTRA_TITLE,"Compartir Imagen")
+
+        }
+        startActivity(Intent.createChooser(intent,null))
+    }
+
     fun compartir(){
         val intent= Intent().apply {
             action= Intent.ACTION_SEND
             putExtra(Intent.EXTRA_TEXT,"COMPARTIR")
             type="text/plain"
+            putExtra(Intent.EXTRA_TITLE,"Pantalla Compartir")
         }
         val compartirIntent= Intent.createChooser(intent,null)
         startActivity(compartirIntent)
